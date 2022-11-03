61a1c9d7cfa730ee63e8c1ddd9fd9c5d778f61d7
Tiffany Cheng <tiffscheng@gmail.com>
2022-11-03 18:10:39 -0500

    Production

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   transpileDependencies: true,
   devServer: {
     open: {
       target: [`http://localhost:8080`]
     }
-  }
+  },
 })
